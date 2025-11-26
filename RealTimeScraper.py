c3a2b65c13ab550e9ca1dfc083b61de22a2ff7eb
Salma001-it <salmanajih001@gmail.com>
2025-11-26 18:55:38 +0100

    Update RealTimeScraper.py

--- RealTimeScraper.py
+++ RealTimeScraper.py
@@ -9,9 +9,9 @@ import time
 import pandas as pd
 import csv
 options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+#options.add_argument("--headless")
+#options.add_argument("--no-sandbox")
+#options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
 # Data e ora attuali
 ora_oggi = datetime.now()  # restituisce un oggetto datetime
