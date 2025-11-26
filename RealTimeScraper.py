35e12b2634a6e6a0fa7eb91e2ba9f4770ab89def
Salma001-it <salmanajih001@gmail.com>
2025-11-26 19:00:55 +0100

    Update RealTimeScraper.py

--- RealTimeScraper.py
+++ RealTimeScraper.py
@@ -9,7 +9,7 @@ import time
 import pandas as pd
 import csv
 options = Options()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 #options.add_argument("--no-sandbox")
 #options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
